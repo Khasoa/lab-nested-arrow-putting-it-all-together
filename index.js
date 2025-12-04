c3ae1229bf726b530e523c765c9c60d7b659e807
Lydia Khasoa <khasoalydia@gmail.com>
2025-12-05 02:31:48 +0300

    changes committed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const login = createLoginTracker({username:"user1", password:"password123" })
 
 function createLoginTracker (userInfo) {
-let attemptCount = 0;
+let attemptCount = 3;
 
 const attemptLogin = (passwordAttempt) => {
 
@@ -13,9 +13,12 @@ if(passwordAttempt===userInfo.password && attemptCount<=3) {
 //if wrong password is used, increase attempt count
 if (passwordAttempt !== userInfo.password) {
 attemptCount++;
+console.log("Status: Password did not match")
+return "Attempt" + attemptCount + ": Login failed";
 
 //lock account after 3 failed attempts
 if(attemptCount >3){
+  console.log("Status: Account locked")
   return "Account locked due to too many failed login attempts"
 }
 return "Attempt " + attemptCount + ": Login failed"
