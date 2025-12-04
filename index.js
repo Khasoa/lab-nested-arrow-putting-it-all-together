f90f7abc72c3536dfbd579fd9e863f1500490d44
Lydia Khasoa <khasoalydia@gmail.com>
2025-12-04 03:39:02 +0300

    Login tracker system

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const login = createLoginTracker({username:"user1", password:"password123" })
 
 function createLoginTracker (userInfo) {
-let attemptCount = 3;
+let attemptCount = 0;
 
 const attemptLogin = (passwordAttempt) => {
-attemptCount++;
 
-if(passwordAttempt===userInfo.password && attemptCount<=3) { 
+//first, check if already locked
+if (attemptCount>=3) {
+  console.log("Status: Account locked")
+  return "Account locked due to too many failed login attempts";
+}
+//When correct password is used
+if(passwordAttempt===userInfo.password) { 
   console.log("Status: Password matched");
   return "Login Successful";
 }
-
-if (passwordAttempt!==userInfo.password && attemptCount <=3){ 
+//Increase attempts when wrong passwords are used
+attemptCount++;
+if (passwordAttempt!==userInfo.password){ 
   console.log("Status: Password did not match");
   return "Attempt " + attemptCount + ": Login failed";
 
 }
 
-if (passwordAttempt!==userInfo.password && attemptCount>=3) {
-  console.log("Status: Account locked")
-  return "Account locked due to too many failed login attempts";
-}
+
 };
 
 return attemptLogin;
 
 }
 
-console.log(login("password"))
+console.log(login("password123"))
 
 
 module.exports = {
