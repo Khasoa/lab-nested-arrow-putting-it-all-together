37cf398ed1edfd276190e8014c38a995893fafca
Lydia Khasoa <khasoalydia@gmail.com>
2025-12-05 03:07:48 +0300

    final login tracker committed

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const login = createLoginTracker({username:"user1", password:"password123" })
 
 function createLoginTracker (userInfo) {
-let attemptCount = 3;
+let attemptCount = 0;
 
 const attemptLogin = (passwordAttempt) => {
 
   //Allow correct Login immediately or after failed attempts
-if(passwordAttempt===userInfo.password && attemptCount<=3) { 
+if(passwordAttempt === userInfo.password) { 
   console.log("Status: Password matched");
   return "Login Successful";
 }
 //if wrong password is used, increase attempt count
-if (passwordAttempt !== userInfo.password) {
 attemptCount++;
-console.log("Status: Password did not match")
-return "Attempt" + attemptCount + ": Login failed";
+console.log("Status: Password did not match");
 
 //lock account after 3 failed attempts
-if(attemptCount >3){
-  console.log("Status: Account locked")
-  return "Account locked due to too many failed login attempts"
+if(attemptCount >= 3) {
+  console.log("Status: Account locked");
+  return "Account locked due to too many failed login attempts";
 }
-return "Attempt " + attemptCount + ": Login failed"
+//otherwise, return failed attempt
+return "Attempt " + attemptCount + ": Login failed";
 }
 
+return attemptLogin;
 };
 
-return attemptLogin
 
-}
 
 console.log(login("password123"))
 
